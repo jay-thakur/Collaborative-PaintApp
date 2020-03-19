da9020ae4708961a89b2e8a1cf2bb3fd8a9ebb20
Jay Prakash Thakur <jayprakashthakursnr@gmail.com>
2020-03-19 11:41:54 +0530

    initializing collaborative-paintapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasDirective } from './canvas.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasDirective
   ],
   imports: [
     BrowserModule,
